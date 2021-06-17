9f7f71f74b9196e842a2a9ba22c3174e67bbf374
Aditya Pathak <mynewchilly@gmail.com>
2021-06-17 15:28:04 +0530

    re

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -1,20 +1,28 @@
 let root = document.querySelector("ul");
 let input = document.querySelector(`input`);
 
+let createImage = (data) => {
+  for (let i = 0; i < data.total; i++) {
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    img.src = data.results[i].urls.small;
+    li.append(img);
+    root.append(li);
+  }
+};
 input.onkeyup = (event) => {
   let search = event.target.value;
   if (event.keyCode === 13) {
     root.innerHTML = "";
     let url = `https://api.unsplash.com/search/photos?&query=${search}&client_id=tZmk05SGRoWrbFqdOcpLEK_rC2oeHq2DifSNLC4aCx8`;
     let data = fetch(url)
-      .then((data) => console.log(data))
+      .then((data) => createImage(data))
       .catch((error) => {
         console.log(error);
       })
       .finally(() => console.log(`done`));
     event.target.value = "";
   }
-  data();
 };
 let fetch = (url) => {
   return new Promise((resolve, reject) => {
@@ -22,14 +30,6 @@ let fetch = (url) => {
     xhr.open(`GET`, url);
     xhr.onload = () => {
       let data = JSON.parse(xhr.response);
-      for (let i = 0; i < data.total; i++) {
-        let li = document.createElement("li");
-        let img = document.createElement("img");
-        img.src = data.results[i].urls.small;
-        li.append(img);
-        root.append(li);
-      }
-
       return resolve(data);
     };
     xhr.onerror = () => {
